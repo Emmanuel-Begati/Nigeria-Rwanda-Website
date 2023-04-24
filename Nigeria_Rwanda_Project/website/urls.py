cf98bc7888b49845e517036cfaad5195a2eb1803
Pascal Onuoha <116768306+iPascal619@users.noreply.github.com>
2023-04-24 19:50:06 +0200

    Update urls.py
    another update

--- Nigeria_Rwanda_Project/website/urls.py
+++ Nigeria_Rwanda_Project/website/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 path ('', views.home, name='website-home')
 , path ('about', views.home, name='website-about'),
 path ('contact-us', views.home, name='website-contact'),
-path ('login', views.home, name='website-login'),
+path('login/', views.login_view, name='website-login')
 path ('sign-up', views.home, name='website-sign-up')
 ]
