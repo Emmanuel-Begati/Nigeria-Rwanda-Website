cb1bdc0027d2d151de1e66a3cb28aec189bf06e8
Pascal Onuoha <116768306+iPascal619@users.noreply.github.com>
2023-04-24 19:38:06 +0200

    Update views.py
    updated views

--- Nigeria_Rwanda_Project/website/views.py
+++ Nigeria_Rwanda_Project/website/views.py
@@ -11,6 +11,22 @@ def contact(request):
 def about(request):
     return render(request, 'website/about.html')
 def login(request):
+     if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid login credentials'})
+    else:
+        return render(request, 'login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('login')
     return render(request, 'website/login.html')
 def sign_up(request):
     return render(request, 'website/sign_up.html')
