dc2b8d96590899f82226837891cb9042aea1f6d2
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-04-26 02:20:51 +0200

    sign up refixed

--- Nigeria_Rwanda_Project/website/urls.py
+++ Nigeria_Rwanda_Project/website/urls.py
@@ -7,9 +7,5 @@ urlpatterns = [
 path ('', views.home, name='website-home')
 , path ('about', views.about, name='website-about'),
 path ('contact-us', views.contact, name='website-contact'),
-path('login/', views.login, name='website-login'),
-<<<<<<< HEAD
-path ('sign_up', views.sign_up, name='website-sign_up'),
-=======
+path('login/', views.login, name='website-login'),]
 path ('sign-up', views.sign_up, name='website-sign-up')
->>>>>>> parent of 724bec6 (sign up issue fixed)]
